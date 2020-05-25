1ebf913b1fc8dca53fbd85627d585e743178aac2
Daniel <daniel.andersson@iths.se>
2020-05-25 13:39:13 +0200

    init commit

---
+++ ADQuiz/Models/Context.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADQuiz
+{
+    public class Context : IdentityDbContext<User>
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+        }
+        
+
+    }
+}
---
+++ ADQuiz/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ADQuiz
+{
+    public class User : IdentityUser
+    {
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+    }
+}
