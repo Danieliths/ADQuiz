725a15755d34e08383b310a6a13abfe1576182b2
Daniel <daniel.andersson@iths.se>
2020-05-27 10:16:46 +0200

    mid work

--- ADQuiz/ClientApp/src/components/NavMenu.js
+++ ADQuiz/ClientApp/src/components/NavMenu.js
@@ -5,14 +5,15 @@ import './NavMenu.css';
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
-
+  static timer;
   constructor (props) {
     super(props);
 
     this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
         collapsed: true,
-        loggedin: ""
+        loggedin: 300,
+        bajs: ""
     };
   }
     
@@ -22,55 +23,67 @@ export class NavMenu extends Component {
     });
   }
     componentDidMount() {
-        this.timer = setInterval(() => this.getItems(), 1000);
+        this.componentNavbarIfLoggedin();
+        this.getData();
     }
 
     componentWillUnmount() {
         this.timer = null; // here...
     }
-
-    getItems() {
-        fetch("/loggedin")
+    getData = () => {
+        fetch('/loggedin')
             .then(response => response.status)
             .then(body => this.setState({ loggedin: body }));
+                this.intervalID = setTimeout(this.getData.bind(this), 5000);
     }
-
-  render () {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/">ADQuiz</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-                </NavItem>
-                
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink tag={Link} className="text-dark" to="/register">Register</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink tag={Link} className="text-dark" to="/login">Login</NavLink>
-                            </NavItem>
-                            if (this.state.loggedin == "200") {
-                                <NavItem>
-                                    <NavLink tag={Link} className="text-dark" to="/login">BAJS</NavLink>
-                                </NavItem>
-                            }
-                            
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    );
-  }
+    componentNavbarIfLoggedin() {
+        if (this.loggedin == 200) {
+            this.bajs = <NavItem>
+                <NavLink tag={Link} className="text-dark" to="/login">BAJS</NavLink>
+            </NavItem>;
+        }
+        else {
+            this.bajs = "";
+        }
+    }
 }
+
+
+render()
+{
+    return (
+        <header>
+            <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+                <Container>
+                    <NavbarBrand tag={Link} to="/">ADQuiz</NavbarBrand>
+                    <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                    <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+                        <ul className="navbar-nav flex-grow">
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                            </NavItem>
+
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/register">Register</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink tag={Link} className="text-dark" to="/login">Login</NavLink>
+                            </NavItem>
+                            
+
+                        </ul>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    );
+}
+
+
+
